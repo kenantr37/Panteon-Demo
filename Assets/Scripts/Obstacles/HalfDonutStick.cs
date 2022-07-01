93af3268641b831fb4a17aaf8da6258c061e9c8d
kenantr37 <44119225+kenantr37@users.noreply.github.com>
2022-07-01 10:23:40 +0300

    Half Donut Done

--- Assets/Scripts/Obstacles/HalfDonutStick.cs
+++ Assets/Scripts/Obstacles/HalfDonutStick.cs
@@ -6,28 +6,27 @@ public class HalfDonutStick : MonoBehaviour
 {
     [SerializeField] float stickSpeed;
     Rigidbody stickRb;
-
     void Awake()
     {
         stickRb = gameObject.GetComponent<Rigidbody>();
     }
-
     void FixedUpdate()
     {
         DonutMovement();
     }
     void DonutMovement()
     {
+
         if (gameObject.transform.localPosition.x >= 0.130f)
         {
-            AddForceToStick(-1);
+            AddForceToStick(-1f);
         }
-        if (gameObject.transform.localPosition.x <= -0.095f)
+        if (gameObject.transform.localPosition.x <= -0.116f)
         {
-            AddForceToStick(1);
+            AddForceToStick(1f);
         }
     }
-    void AddForceToStick(int direction)
+    void AddForceToStick(float direction)
     {
         stickRb.AddForce(direction * stickSpeed * 10 * Time.deltaTime, 0, 0);
     }
