c6dc5a02214afe729c5b30787f9ee93cc9d9f8d0
kenantr37 <44119225+kenantr37@users.noreply.github.com>
2022-07-02 17:02:15 +0300

    Opponent Movement Trial v.2

--- Assets/Scripts/Opponent/Opponent.cs
+++ Assets/Scripts/Opponent/Opponent.cs
@@ -7,18 +7,9 @@ public class Opponent : MonoBehaviour
     [SerializeField] float opponentMoveForwardSpeed = .5f;
     [SerializeField] List<Transform> objectsToEscape;
     [SerializeField] float calculateHypotenuse;
+    [SerializeField] GameObject wayPoint;
 
-    GameObject ground;
-    Rigidbody opponentRb;
-    float groundWeightSize;
-
-    void Start()
-    {
-        groundWeightSize = GameObject.FindGameObjectWithTag("Ground").GetComponent<BoxCollider>().size.z;
-        ground = GameObject.FindGameObjectWithTag("Ground");
-        opponentRb = GetComponent<Rigidbody>();
-    }
-
+    float opponentXBorder = 0.403f;
     void Update()
     {
         OpponentMoveForward();
@@ -28,8 +19,9 @@ public class Opponent : MonoBehaviour
     }
     void OpponentMoveForward()
     {
-        Vector3 opponentForward = Vector3.forward * opponentMoveForwardSpeed * Time.deltaTime;
-        transform.position = transform.position + opponentForward;
+        //Vector3 opponentForward = new Vector3(0, transform.localPosition.y, 1) * Time.deltaTime * opponentMoveForwardSpeed;
+        //transform.Translate(opponentForward);
+        transform.position = Vector3.MoveTowards(transform.localPosition, wayPoint.transform.position, Time.deltaTime * opponentMoveForwardSpeed);
         transform.rotation = Quaternion.Euler(Vector3.forward);
     }
     void OpponentMoveLeftRight()
@@ -38,45 +30,33 @@ public class Opponent : MonoBehaviour
         {
             float verticalDistanceBetweenNextObject = Mathf.Abs(transform.position.z - everyObject.position.z);
             float horizontalDistanceBetweenNextObject = Mathf.Abs(transform.localPosition.x - everyObject.position.x);
-            //Debug.Log("yatay mesafe" + horizontalDistanceBetweenNextObject);
 
-            if (verticalDistanceBetweenNextObject <= .3f)
+            if (verticalDistanceBetweenNextObject <= 1)
             {
-                //Debug.DrawLine(transform.position, everyObject.position, Color.green);
+                Debug.DrawLine(transform.position, everyObject.position, Color.green);
                 calculateHypotenuse = Mathf.Sqrt(Mathf.Pow(horizontalDistanceBetweenNextObject, 2) + Mathf.Pow(verticalDistanceBetweenNextObject, 2));
 
-                if (calculateHypotenuse <= .3f && everyObject.position.x <= transform.localPosition.x)
+                if (calculateHypotenuse <= .5f && everyObject.position.x <= transform.localPosition.x)
                 {
-                    Debug.Log("saðdan geçti");
-                    transform.Translate(Vector3.right * Time.deltaTime * .5f);
-                    continue;
+                    Debug.DrawLine(transform.position, everyObject.position, Color.red);
+                    transform.Translate(Vector3.right * Time.deltaTime * 3);
                 }
-                else if (calculateHypotenuse <= .3f && everyObject.position.x > transform.localPosition.x)
+                else if (calculateHypotenuse <= .5f && everyObject.position.x > transform.localPosition.x)
                 {
-                    transform.Translate(Vector3.left * Time.deltaTime * .3f);
-                    continue;
-                    Debug.Log("soldan geçti");
-
+                    Debug.DrawLine(transform.position, everyObject.position, Color.red);
+                    transform.Translate(Vector3.left * Time.deltaTime * 3);
                 }
             }
         }
     }
     void OpponentKeepStayingOnTheWay()
     {
-        if (transform.position.x <= -groundWeightSize)
+        //float opponentWayBorderx = Mathf.Clamp(transform.localPosition.x, -0.482f, 0.482f);
+        //transform.localPosition = new Vector3(opponentWayBorderx, transform.localPosition.y, transform.localPosition.z);
+
+        if (transform.localPosition.x <= -opponentXBorder)
         {
-            transform.position = Vector3.MoveTowards(new Vector3(transform.position.x, transform.position.y, transform.position.z),
-                Vector3.zero, Time.deltaTime * .5f);
+            transform.Translate(0, 0, transform.localPosition.z);
         }
-        //else if (transform.position.x >= groundWeightSize)
-        //{
-        //    transform.position = Vector3.MoveTowards(new Vector3(groundWeightSize, transform.position.y, transform.position.z),
-        //        Vector3.zero, Time.deltaTime * .5f);
-        //}
-        //else
-        //{
-        //    transform.position = Vector3.MoveTowards(new Vector3(groundWeightSize, transform.position.y, transform.position.z),
-        //        Vector3.zero, Time.deltaTime * .5f);
-        //}
     }
 }
