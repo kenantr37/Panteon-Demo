7171a9bbea0ff216d4a2b5a495b1561f5d722363
kenantr37 <44119225+kenantr37@users.noreply.github.com>
2022-07-08 02:42:08 +0300

    Wall Percentage v.2

--- Assets/Prefabs/My_Prefabs/Wall/Brush.cs
+++ Assets/Prefabs/My_Prefabs/Wall/Brush.cs
@@ -1,24 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class Brush : MonoBehaviour
 {
     [SerializeField] GameObject brush;
     [SerializeField] float brushSize;
+    [SerializeField] TextMeshProUGUI rayX, rayY;
     public bool canPaint;
 
     Vector3 firstPosition;
     Vector3 secondPosition;
     Vector3 isCursorMoved;
     RaycastHit hit;
-
+    public int counter = 0;
     void Start()
     {
         canPaint = true;
     }
     void Update()
     {
+
         if (Input.GetMouseButton(0))
         {
             firstPosition = Input.mousePosition;
@@ -28,11 +30,11 @@ public class Brush : MonoBehaviour
             secondPosition = Input.mousePosition;
             isCursorMoved = secondPosition - firstPosition;
 
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
             if (isCursorMoved != firstPosition)
             {
-                var Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-                if (Physics.Raycast(Ray, out hit))
+                if (Physics.Raycast(ray, out hit))
                 {
                     if (Input.mousePosition.x > 350 && Input.mousePosition.x < 950 && canPaint)
                     {
--- Assets/Prefabs/My_Prefabs/Wall/Percentage.cs
+++ Assets/Prefabs/My_Prefabs/Wall/Percentage.cs
@@ -6,23 +6,47 @@ public class Percentage : MonoBehaviour
 {
     ScoreManager scoreManager;
     bool artýrýldý;
-
+    GameObject[] brush;
     void Awake()
     {
         scoreManager = FindObjectOfType<ScoreManager>();
     }
 
-
+    void Update()
+    {
+        BrushDeleter();
+    }
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Brush") && !artýrýldý)
         {
-            scoreManager.initialCountNumber += 1;
             artýrýldý = true;
+            scoreManager.initialCountNumber += 1;
             StartCoroutine(IncreasePercentage());
             StartCoroutine(DestroyPercentage());
         }
     }
+
+    void BrushDeleter()
+    {
+        brush = GameObject.FindGameObjectsWithTag("Brush");
+
+        if (brush != null)
+        {
+            for (int i = 0; i < brush.Length - 1; i++) // I made this to ristrict amount of brushes
+            {
+                if (brush[i + 1].transform.position == brush[i].transform.position)
+                {
+                    Destroy(brush[i + 1]);
+                }
+            }
+        }
+        else
+        {
+            return;
+        }
+    }
+
     IEnumerator IncreasePercentage()
     {
         yield return new WaitForSeconds(.4f);
